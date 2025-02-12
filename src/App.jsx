86d2b1bc7a4af8d8339779e989e7692be83eb360
George-25 <georgemallory26@gmail.com>
2025-02-11 23:25:32 -0500

    created props and add some items into it

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,24 @@ import Accordion from "./components/Accordion";
 const App = () => {
   const items = [
     {
+      id: "l1react",
       label: "Can I use React on a project?",
       content: "You can use React on any project you wnat.",
     },
     {
+      id: "l2javascript",
       label: "Can I use Javascript on a project?",
       content: "You can use Javascript on any project.",
     },
     {
+      id: "l3CSS",
       label: "Can I use CSS on a project?",
       contect: "You can use CSS on any project.",
     },
   ];
   return (
     <>
-      <Accordion  items={items}/>
+      <Accordion items={items} />
     </>
   );
 };
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Accordion({ items }) {
-  return <div></div>;
+  const renderItems = items.map((item) => {
+    return (
+      <div key={item.id}>
+        <div>{item.label}</div>
+        <div>{item.content}</div>
+      </div>
+    );
+  });
+  return <div>{renderItems}</div>;
 }
 
 export default Accordion;
