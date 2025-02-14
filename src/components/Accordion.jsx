218d136a5757969ed735ed690fc916350203130b
George-25 <georgemallory26@gmail.com>
2025-02-13 21:18:51 -0500

    some changes in Accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,16 +4,15 @@ function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
   const renderItems = items.map((item, index) => {
+    // create state here!
     const isExpanded = index === expandedIndex;
+    // create event handler !
+    const handleClick = () => {
+      setExpandedIndex(index);
+    };
     return (
       <div key={item.id}>
-        <div
-          onClick={() => {
-            setExpandedIndex(index);
-          }}
-        >
-          {item.label}
-        </div>
+        <div onClick={handleClick}>{item.label}</div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
