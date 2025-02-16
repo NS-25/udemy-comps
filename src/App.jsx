a91e84c7caa4e1c0e4e056636bebe97eeab526f7
George-25 <georgemallory26@gmail.com>
2025-02-16 13:02:30 -0500

    created dropdown and remove some codes from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import React from "react";
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
 const App = () => {
-  const items = [
-    {
-      id: "l1react",
-      label: "Can I use React on a project?",
-      content: "You can use React on any project you want.",
-    },
-    {
-      id: "l2javascript",
-      label: "Can I use Javascript on a project?",
-      content: "You can use Javascript on any project.",
-    },
-    {
-      id: "l3CSS",
-      label: "Can I use CSS on a project?",
-      content: "You can use CSS on any project.",
-    },
-  ];
-  const handleItems = (item) => {
-    console.log(item);
-  };
-
+  
+  
   return (
     <>
-      <Accordion items={items} />
+<Dropdown/>
     </>
   );
 };
---
+++ src/pages/AccordionPage.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Accordion from "../components/Accordion";
+
+const AccordionPage = () => {
+  const items = [
+    {
+      id: "l1react",
+      label: "Can I use React on a project?",
+      content: "You can use React on any project you want.",
+    },
+    {
+      id: "l2javascript",
+      label: "Can I use Javascript on a project?",
+      content: "You can use Javascript on any project.",
+    },
+    {
+      id: "l3CSS",
+      label: "Can I use CSS on a project?",
+      content: "You can use CSS on any project.",
+    },
+  ];
+  const handleItems = (item) => {
+    console.log(item);
+  };
+
+  return (
+    <>
+      <Accordion items={items} />
+    </>
+  );
+};
+
+export default AccordionPage;
