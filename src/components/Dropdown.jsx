9146c5a524998e2635034369c7a6eb598e591a82
George-25 <georgemallory26@gmail.com>
2025-02-19 23:56:42 -0500

    some codes updated in dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,14 @@
-const Dropdown = ({options}) => {
-  
-  return <div>Dropdown...</div>;
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return <div key={option.value}>{option.label}</div>;
+  });
+
+  return (
+    <div>
+      <div>Select...</div>
+      <div>{renderedOptions}</div>
+    </div>
+  );
 };
 
 export default Dropdown;
