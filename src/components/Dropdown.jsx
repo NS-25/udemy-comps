f4bd7e65d0fc573ef116f4c167e995efafa35bc9
George-25 <georgemallory26@gmail.com>
2025-03-20 23:32:17 -0400

    some updates in dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Dropdown = ({ options, selection, onSelect }) => {
+const Dropdown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
@@ -17,7 +17,7 @@ const Dropdown = ({ options, selection, onSelect }) => {
     // CLOSE DROPDOWN
     setIsOpen(!isOpen);
     // WHAT OPTION DID THE USER CLICK ON ???
-    onSelect(option);
+    onChange(option);
   };
 
   const renderedOptions = options.map((option) => {
@@ -35,7 +35,7 @@ const Dropdown = ({ options, selection, onSelect }) => {
 
   return (
     <div>
-      <div onClick={handleClick}>{selection?.label || "Select..." }</div>
+      <div onClick={handleClick}>{value?.label || "Select..."}</div>
       {isOpen && <div>{renderedOptions}</div>}
     </div>
   );
