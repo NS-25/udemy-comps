036e63e0e31af16ada8ea34fcd9456bb1a827ae0
George-25 <georgemallory26@gmail.com>
2025-04-06 00:25:12 -0400

    added useEffect in navigation jsx

--- src/context/navigation.jsx
+++ src/context/navigation.jsx
@@ -1,9 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 const NavigationProvider = ({ children }) => {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  // useEffect section
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener("popstate", handler);
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
+
   return (
     <NavigationContext.Provider value={{}}>
       {currentPath}
