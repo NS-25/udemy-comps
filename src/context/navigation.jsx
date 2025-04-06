3fab7318cd310818ba02f74df13d91204378f75c
George-25 <georgemallory26@gmail.com>
2025-04-05 23:56:00 -0400

    wrote window location to check th path

--- src/context/navigation.jsx
+++ src/context/navigation.jsx
@@ -3,9 +3,10 @@ import { createContext, useState } from "react";
 const NavigationContext = createContext();
 
 const NavigationProvider = ({ children }) => {
-  const [currentPath,setCurrentPath] = useState();
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
   return (
     <NavigationContext.Provider value={{}}>
+      {currentPath}
       {children}
     </NavigationContext.Provider>
   );
