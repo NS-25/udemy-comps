3e2822a6b0db38593a274b142b1659596f3ae505
George-25 <georgemallory26@gmail.com>
2025-04-05 23:51:24 -0400

    added Navigation provider in index jsx

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import '../src/index.css';
+import { NavigationProvider } from "./context/navigation.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <NavigationProvider>
+      <App /> 
+    </NavigationProvider>
+
   </StrictMode>
 );
