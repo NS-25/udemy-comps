2a83d668ecf150a9f6c80d0de9c7168c21b874fa
George-25 <georgemallory26@gmail.com>
2025-04-06 17:25:21 -0400

    created navigate in navigation jsx

--- src/context/navigation.jsx
+++ src/context/navigation.jsx
@@ -16,6 +16,11 @@ const NavigationProvider = ({ children }) => {
     };
   }, []);
 
+  const navigate = (to) => {
+    window.history.pushState({}, "", to);
+    setCurrentPath(to);
+  };
+
   return (
     <NavigationContext.Provider value={{}}>
       {currentPath}
