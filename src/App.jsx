a18f7570ade10ea00e90dfec71060fdf8c567dca
George-25 <georgemallory26@gmail.com>
2025-04-06 22:10:07 -0400

    created route jsx in comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-
+import Link from "./components/Link";
 import ModalPage from "./pages/ModalPage";
 
 const App = () => {
   return (
     <>
+      <div>
+        <Link to="/accordion">Go to accordion</Link>
+        <Link to="/dropdown">Go to dropdown</Link>
+      </div>
       <div>
         <ModalPage />
       </div>
