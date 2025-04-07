ac16921f83b94544fe8f4f828ab4f75a6b4ac8dc
George-25 <georgemallory26@gmail.com>
2025-04-06 22:14:51 -0400

    added routes in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
 import ModalPage from "./pages/ModalPage";
 
 const App = () => {
@@ -8,6 +11,14 @@ const App = () => {
       <div>
         <Link to="/accordion">Go to accordion</Link>
         <Link to="/dropdown">Go to dropdown</Link>
+      </div>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
       </div>
       <div>
         <ModalPage />
