d35c43f1258ff49c84881ac18fa427db146bddc6
George-25 <georgemallory26@gmail.com>
2025-04-07 20:39:46 -0400

    added some tailwind css for main div in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,26 @@ import AccordionPage from "./pages/AccordionPage";
 import DropdownPage from "./pages/DropdownPage";
 import ModalPage from "./pages/ModalPage";
 import ButtonPage from "./pages/ButtonPage";
+
 const App = () => {
   return (
     <>
-      <div>
+      <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
         <Sidebar />
-      </div>
-      <div>
-        <Route path="/accordion">
-          <AccordionPage />
-        </Route>
-        <Route path="/">
-          <DropdownPage />
-        </Route>
-        <Route path="/buttons">
-          <ButtonPage />
-        </Route>
-      </div>
-      <div>
-        <ModalPage />
+        <div className="col-span-5">
+          <Route path="/accordion">
+            <AccordionPage />
+          </Route>
+          <Route path="/">
+            <DropdownPage />
+          </Route>
+          <Route path="/buttons">
+            <ButtonPage />
+          </Route>
+        </div>
+        <div>
+          <ModalPage />
+        </div>
       </div>
     </>
   );
