af62a1ae1fa058b6dc2612b3f466f6f274b4c587
George-25 <georgemallory26@gmail.com>
2025-04-07 20:13:12 -0400

    added map functio and some codes for better performance

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,20 @@
 import Link from "./Link";
 
 const Sidebar = () => {
-  return (
-    <div>
-      <Link />
-    </div>
-  );
+  const links = [
+    { label: "Dropdown", path: "/" },
+    { label: "Accordion", path: "/accordion" },
+    { label: "Buttons", path: "/buutons" },
+  ];
+
+  const renderLinks = links.map(() => {
+    return (
+      <Link key={Link.label} to={links.path}>
+        {links.label}
+      </Link>
+    );
+  });
+  return <div>{renderLinks}</div>;
 };
 
 export default Sidebar;
