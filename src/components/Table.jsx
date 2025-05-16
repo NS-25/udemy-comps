a7bce8089fe1d92c669428817ef034ef5fb94143
George-25 <georgemallory26@gmail.com>
2025-05-15 23:16:18 -0400

    added some code in table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,18 +2,24 @@ import React from "react";
 
 const Table = ({ data }) => {
   const renderRows = data.map((fruit) => {
-    <tr key={fruit.name}>
-      <td>{fruit.name}</td>
-      <td>{fruit.color}</td>
-      <td>{fruit.score}</td>
-    </tr>;
+    return (
+      <tr key={fruit.name}>
+        <td>{fruit.name}</td>
+        <td>{fruit.color}</td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
   });
 
   return (
     <table>
       <thead>
-        <tr>Fruit</tr>
-        <tr>Color</tr>
+        <tr>
+          <th>Fruit</th>
+        </tr>
+        <tr>
+          <th>Color</th>
+        </tr>
         <tr>Score</tr>
       </thead>
       <tbody>{renderRows}</tbody>
