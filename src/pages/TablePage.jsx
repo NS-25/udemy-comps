f64836c10f740b0e906a74d4c57db5c8808264ea
George-25 <georgemallory26@gmail.com>
2025-05-15 22:26:40 -0400

    added some update

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -4,9 +4,9 @@ import Table from "../components/Table";
 const TablePage = () => {
   const data = [
     { name: "orange", color: "bg-orange-500", score: 5 },
-    { name: "orange", color: "bg-orange-500", score: 5 },
-    { name: "orange", color: "bg-orange-500", score: 5 },
-    { name: "orange", color: "bg-orange-500", score: 5 },
+    { name: "apple", color: "bg-red-500", score: 3 },
+    { name: "banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
   ];
   return (
     <div>
