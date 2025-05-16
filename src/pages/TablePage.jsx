811173e1a3d1251333c220c72f817695c2af70fe
George-25 <georgemallory26@gmail.com>
2025-05-15 22:25:06 -0400

    added some objects in table comp

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import Table from "../components/Table";
 
 const TablePage = () => {
+  const data = [
+    { name: "orange", color: "bg-orange-500", score: 5 },
+    { name: "orange", color: "bg-orange-500", score: 5 },
+    { name: "orange", color: "bg-orange-500", score: 5 },
+    { name: "orange", color: "bg-orange-500", score: 5 },
+  ];
   return (
     <div>
       <Table />
